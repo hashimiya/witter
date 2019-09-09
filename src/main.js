610e89e4a4674e69d231d899859f1a6ead124377
howyi <howyi.lq@gmail.com>
2019-09-09 13:34:08 +0900

    amplifyとvueのつなぎこみを一時的に削除

--- src/main.js
+++ src/main.js
@@ -6,11 +6,11 @@ import store from './store'
 Vue.config.productionTip = false
 
 // https://aws-amplify.github.io/docs/js/vue#configuration
-import Amplify, * as AmplifyModules from 'aws-amplify'
-import { AmplifyPlugin } from 'aws-amplify-vue'
-import awsconfig from './aws-exports'
-Amplify.configure(awsconfig)
-Vue.use(AmplifyPlugin, AmplifyModules)
+// import Amplify, * as AmplifyModules from 'aws-amplify'
+// import { AmplifyPlugin } from 'aws-amplify-vue'
+// import awsconfig from './aws-exports'
+// Amplify.configure(awsconfig)
+// Vue.use(AmplifyPlugin, AmplifyModules)
 
 new Vue({
   router,
